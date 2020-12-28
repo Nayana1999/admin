b8fde3d2da269a57f0c7dccdb33b8d7688cb0cf7
Nayana-1999 <nayanasaara@gmail.com>
2020-12-29 00:50:12 +0530

    My first commit

---
+++ admin/head.php
@@ -0,0 +1,8 @@
+<nav class = "navbar navbar-default navbar-fixed-top">
+	<div class = "container-fluid">
+		<a class="navbar-brand" href="#">
+		
+		</a>
+		<a class = "navbar-brand">Admin Panel </a>
+	<div>
+</nav>
---
+++ head.php
@@ -0,0 +1,6 @@
+<nav class = "navbar navbar-default navbar-fixed-top">
+	<div class = "container-fluid">
+			
+		<a href="index.php" class = "navbar-brand"><center>Welcome to Admin Panel</center></a>
+	<div>
+</nav>
